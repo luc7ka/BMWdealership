aa8eb8eeb4c52920ff8977768ab6d16f52effd3c
Lucija Dujmić <99915448+luc7ka@users.noreply.github.com>
2022-06-07 21:40:15 +0200

    Create prijava_Admin.c

---
+++ prijava_Admin.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+#include <stdbool.h>
+#include <string.h>
+
+bool prijava_Admin(ADMIN* admin_pok) {
+	ADMIN unos;
+	printf("Unesi korisnicko ime:\n  ");
+	scanf("%s", &unos.korisnicko_ime);
+	printf("Unesi Lozinku:\n  ");
+	scanf("%s", &unos.lozinka);
+	if (strcmp(admin_pok->korisnicko_ime, unos.korisnicko_ime) == 0 && strcmp(admin_pok->lozinka, unos.lozinka) == 0)
+		return true;
+	else
+		return false;
+};
